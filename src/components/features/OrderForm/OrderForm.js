c5b3d9f442dd37a2ea53301a50b543f5e71dcc0f
Mateusz Krupa <mateuszandrzejkrupa@gmail.com>
2021-02-22 18:26:55 +0100

    Add orderOptions-add new components

--- src/components/features/OrderForm/OrderForm.js
+++ src/components/features/OrderForm/OrderForm.js
@@ -6,18 +6,15 @@ import OrderSummary from '../OrderSummary/OrderSummary';
 import OrderOption from '../OrderOption/OrderOption';
 import pricing from '../../../data/pricing.json';
 
-const OrderForm = ({tripCost, options}) => (
+const OrderForm = ({tripCost, options }) => (
   <Grid>
     {/* {console.log('props', tripCost)} */}
     <Row>
       {pricing.map((option) => (
         <Col md={4} key={option.id}>
-          {' '}
-          <OrderOption
-            // setOrderOption={setOrderOption}
-            currentValue={options[option.id]}
-            title={option.id}
-            {...option}
+          <OrderOption 
+            name={option.name}
+            type={option.type}
           />
         </Col>
       ))}
--- src/components/features/OrderOption/OrderOption.js
+++ src/components/features/OrderOption/OrderOption.js
@@ -2,15 +2,43 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import styles from './OrderOption.scss';
+// import OrderOptionCheckboxes from './OrderOptionCheckboxes';
+// import OrderOptionDropdown from './OrderOptionDropdown';
+// import OrderOptionIcons from './OrderOptionIcons';
+// import OrderOptionNumber from './OrderOptionNumber';
 
-const OrderOption = ({options}) => (
+// const optionTypes = {
+//   dropdown: OrderOptionDropdown,
+//   icons: OrderOptionIcons,
+//   checkboxes: OrderOptionCheckboxes,
+//   number: OrderOptionNumber,
+// };
+
+// const OrderOption = ({name, type, ...otherProps}) => {
+//     const OptionComponent = optionTypes[type];
+//     if(!OptionComponent){
+//       return null;
+//     } else {
+//       return (
+//         <div className={styles.component}>
+//           <h3 className={styles.title}>{name}</h3>
+//           <OptionComponent
+//             {...otherProps}
+//           />
+//         </div>
+//       );
+//     }
+//   };
+
+const OrderOption = ({name, type}) => (
   <div className={styles.component}>
-    <h3 className={styles.title}>{'options'} {options}</h3>
-  </div>
-);
+    <h3 className={styles.title}>{name} {type}</h3>
+  </div>);
 
 OrderOption.propTypes = {
   options: PropTypes.object,
+  name: PropTypes.string,
+  type: PropTypes.string,
 };
   
 export default OrderOption;
